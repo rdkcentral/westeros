ffdea9d308a254b1b95197f62a2d39ee58f7cbea
MariSelvamV-7 <v.mariselvam0001@gmail.com>
2026-01-09 10:18:37 +0530

    RDKEMW-10871: Merge all L1 Test suite generated for Westeros Main Update westeros-linux-dmabuf.cpp
    Reason for change : Added the L1 test suite for westeros main to generate coverage report. As it has less percentage for westeros-compositor.cpp file other than all have 75% coverage.
    Test Procedure : None
    Priority : P1
    Risks :None
    Signed-off-by : mariselvam_veerakumar@comcast.com

--- linux-dmabuf/westeros-linux-dmabuf.cpp
+++ linux-dmabuf/westeros-linux-dmabuf.cpp
@@ -457,6 +457,10 @@ UNIT_TEST_STATIC void wstLDBBind(struct wl_client *client, void *data, uint32_t 
    printf("westeros-ldb: wstLDBBind: exit: client %p id %d\n", (void*)client, id);
 }
 
+#ifdef UNIT_TEST
+}
+#endif
+
 wl_ldb* WstLDBInit( struct wl_display *display, struct wayland_ldb_callbacks *callbacks, void *userData )
 {
    struct wl_ldb *ldb= 0;
